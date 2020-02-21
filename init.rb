00fdf597afbb1501193398da4a090d2487e88b8c
guillaumequillet <bgachats@yahoo.fr>
2020-02-21 08:20:11 +0100

    initial commit

---
+++ init.rb
@@ -0,0 +1,26 @@
+# MINI JAM 48
+# Theme : Team Work
+# Limitation : The player(s) can not attack!
+
+require 'gosu'
+
+class Window < Gosu::Window
+  def initialize
+    super(640, 480, false)
+    self.caption = 'MINI JAM 48'
+  end
+
+  def button_down(id)
+    close! if id == Gosu::KB_ESCAPE
+  end
+
+  def update
+
+  end
+
+  def draw
+
+  end
+end
+
+Window.new.show
