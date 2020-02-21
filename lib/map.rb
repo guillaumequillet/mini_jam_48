d0594a38ad4f2f2db6ef2f4a24fe8305b87b3da0
guillaumequillet <bgachats@yahoo.fr>
2020-02-21 19:06:00 +0100

    added display list to optimize maps

--- lib/map.rb
+++ lib/map.rb
@@ -1,39 +1,70 @@
 class Map
   def initialize(filename)
-    @tileset ||= GLTexture.load_tiles('gfx/tileset.png', 16, 16)
-    @wallset ||= GLTexture.load_tiles('gfx/wallset.png', 16, 48)
+    @tileset = GLTexture.load_tiles('gfx/tileset.png', 16, 16)
+    @wallset = GLTexture.load_tiles('gfx/wallset.png', 16, 48)
+
+    @models = {
+      desk:    ObjModel.new('desk', true),
+      monitor: ObjModel.new('monitor')
+    }
+
+    read_file(filename)
+  end
+
+  def read_file(filename)
+    transparent_color = Gosu::Color.new(255, 255, 0, 255)
+    @tiles = []
+    @minimap = Gosu::Image.new("maps/#{filename}.png", retro: true)
+    @minimap.height.times do |y|
+      @minimap.width.times do |x|
+        color = @minimap.get_pixel(x, y)
+        unless color == transparent_color
+          @tiles[y * @minimap.width + x] = 0
+        end
+      end
+    end
+  end
+
+  def convert_index_to_coords(i)
+    y = i / @minimap.width
+    x = i % @minimap.width
+    [x, y]
+  end
+
+  def convert_coords_to_index(x, y)
+    @minimap.width * y + x
   end
 
   def draw
-    @desk    ||= ObjModel.new('desk', true)
-    @monitor ||= ObjModel.new('monitor')
-    
-    w, l = 5, 5
-    glBindTexture(GL_TEXTURE_2D, @tileset[1].get_id)
-    glPushMatrix
-    glScalef(w * 16, 1, l * 16)
-    glBegin(GL_QUADS)
-      glTexCoord2d(0, 0); glVertex3f(0, 0, 0)
-      glTexCoord2d(0, l); glVertex3f(0, 0, 1)
-      glTexCoord2d(w, l); glVertex3f(1, 0, 1)
-      glTexCoord2d(w, 0); glVertex3f(1, 0, 0)
-    glEnd
-    glPopMatrix
+    unless defined?(@display_list)
+      @display_list = glGenLists(1)
+      glNewList(@display_list, GL_COMPILE)
 
-    # w, l = 5, 5
-    # glBindTexture(GL_TEXTURE_2D, @tileset[0].get_id)
-    # glPushMatrix
-    # glScalef(w * 16, 48, l * 16)
-    # glBegin(GL_QUADS)
-    #   glTexCoord2d(0, 0); glVertex3f(0, 1, 0)
-    #   glTexCoord2d(0, l); glVertex3f(0, 1, 1)
-    #   glTexCoord2d(w, l); glVertex3f(1, 1, 1)
-    #   glTexCoord2d(w, 0); glVertex3f(1, 1, 0)
-    # glEnd
-    # glPopMatrix
+        last_loaded_texture = nil
 
+        glPushMatrix
+        glScalef(@tileset[0].width, 1, @tileset[1].height)
 
-    @desk.draw(0, 0, 0)  
-    @monitor.draw(18, 10.5, 4)
+        @tiles.each_with_index do |tile, i|
+          next if tile.nil?
+
+          x, z = *convert_index_to_coords(i)
+          if last_loaded_texture.nil? or last_loaded_texture != tile
+            glBindTexture(GL_TEXTURE_2D, @tileset[tile].get_id)
+            last_loaded_texture = tile
+          end
+
+          glBegin(GL_QUADS)
+            glTexCoord2d(0, 0); glVertex3f(x, 0, z)
+            glTexCoord2d(0, 1); glVertex3f(x, 0, z + 1)
+            glTexCoord2d(1, 1); glVertex3f(x + 1, 0, z + 1)
+            glTexCoord2d(1, 0); glVertex3f(x + 1, 0, z)
+          glEnd
+        end
+        glPopMatrix
+      glEndList
+    end
+
+    glCallList(@display_list)
   end
 end
--- lib/utils.rb
+++ lib/utils.rb
@@ -4,7 +4,7 @@ class Gosu::Image
       return nil
     else
       @blob ||= self.to_blob
-      result = @blob[(y * gosu_image.width + x) * 4, 4].unpack("C*")
+      result = @blob[(y * self.width + x) * 4, 4].unpack("C*")
       return Gosu::Color.new(result[3], result[0], result[1], result[2])
     end   
   end
